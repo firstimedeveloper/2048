922ac438b68845ff8fd3ee095e4bbe22353ec56f
Jun Hyuk Han <hanjun.1998@gmail.com>
2020-08-24 17:33:17 +0900

    now able to wait 5000 ms after changing board before pushing a new random number

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
 
 
 const Box = (props) => {
 	return (
 		<div className="flex justify-center items-center w-24 h-24 bg-gray-400 border-2">
 			{props.value}
-		</div>
-	)
-}
-
-const Board = (props) => {
-/* 
- 0  1  2  3
- 4  5  6  7
- 8  9  10 11
- 12 13 14 15
-*/
-
-
-	return (
-		<div className="grid grid-cols-4">
-			{props.board.map((v,i) => {
-					return <Box key={i} value={v}/>
-			})}
 		</div>
 	)
 }
@@ -43,73 +25,87 @@ const Button = (props) => {
 	)
 }
 
+const Board = (props) => {
+/* 
+ 0  1  2  3
+ 4  5  6  7
+ 8  9  10 11
+ 12 13 14 15
+*/	
+	if (!props.wait && props.direction !== "") {
+		const newBoard = [...props.board]
+		for(let i=0;i<4;i++) {
+			let row = []
+			for(let j=0;j<4;j++) {
+				if (props.direction === "left" || props.direction === "right")
+					row.push(props.board[(i*4)+j])
+				if (props.direction === "up" || props.direction === "down")
+					row.push(props.board[(j*4)+i])
+			}
+			if (row != null && row.length > 0) {
+				addRow(row)
+				moveRow(row, props.direction)
+			
+				for(let j=0;j<4;j++) {
+					if (props.direction === "left" || props.direction === "right")
+						newBoard[(i*4)+j] = row[j]
+					if (props.direction === "up" || props.direction === "down")
+						newBoard[(j*4)+i] = row[j]
+				}
+			}	
+		}
+		// if (JSON.stringify(board) !== JSON.stringify(newBoard)) {
+		props.setBoard(newBoard)
+		// }
+		props.setDirection("")
+		props.setWait(true)
+	}
+	
+
+
+	return (
+		<div className="grid grid-cols-4">
+			{props.board.map((v,i) => {
+					return <Box key={i} value={v}/>
+			})}
+		</div>
+	)
+}
+
+
 const App = () => {
 	const [board, setBoard] = useState(() => generateInitialBoard())
 	const [direction, setDirection] = useState("")
+	const [gameOver, setGameOver] = useState(false)
+	const [wait, setWait] = useState(false)
 
-	var gameOver = false
-
+	const timeoutIdRef = useRef();
+	const cancel = useCallback(
+		() => {
+			const timeoutId = timeoutIdRef.current;
+			if (timeoutId) {
+				timeoutIdRef.current = undefined;
+				clearTimeout(timeoutId);
+			}
+		},
+		[timeoutIdRef],
+	);
 	useEffect(() => {
-		const handleMovement = (row, move) => {
-			if (move === "left" || move === "up") {
-				row.sort((a,b) => {
-					if (b>a) {
-					  return 0
-					}
-					if (b==null) {
-					  return -1
-					}
-					else {
-						return 0
-					}
-				});		
-			}
-			if (move === "right" || move === "down") {
-				row.sort((a,b)=>{
-					if (a>b) {
-					  return 0
-					}
-					if (a==null) {
-					  return -1
-					}
-					else {
-						return 0
-					}			
-				});
-			}
+		if (wait) {
+			timeoutIdRef.current = setTimeout(() => {
+				let idx = 0
+				do {
+					idx = Math.floor(Math.random() * 16)
+				} while (board[idx] != null)
+				let num = generateRandomNum()
+				const newBoard = [...board]
+				newBoard[idx] = num
+				setWait(false)
+				setBoard(newBoard)
+			}, 500);
 		}
-
-		if (!gameOver && direction !== "") {
-			const newBoard = [...board]
-			for(let i=0;i<4;i++) {
-				let row = []
-				for(let j=0;j<4;j++) {
-					if (direction === "left" || direction === "right")
-						row.push(board[(i*4)+j])
-					if (direction === "up" || direction === "down")
-						row.push(board[(j*4)+i])
-				}
-				if (row != null && row.length > 0) {
-					//handleAddition
-					console.log(i, row)
-					handleMovement(row, direction)
-				
-					for(let j=0;j<4;j++) {
-						console.log(i, row)
-						if (direction === "left" || direction === "right")
-							newBoard[(i*4)+j] = row[j]
-						if (direction === "up" || direction === "down")
-							newBoard[(j*4)+i] = row[j]
-					}
-				}	
-			}
-			
-			setBoard(newBoard)
-			setDirection("")
-		}
-
-	}, [gameOver, board, direction])
-
+		return cancel;
+	}, [cancel, board, wait]);	
 
 
 	// resets board
@@ -117,63 +113,70 @@ const App = () => {
 		setBoard(() => generateInitialBoard())
 	}
 
-	// button that triggers the next round
-	const nextRound = () => {
-		if (!gameOver) {
-			let idx = 0
-			do {
-				idx = Math.floor(Math.random() * 16)
-			} while (board[idx] != null)
-			let num = generateRandomNum()
-			handleUpdate(idx, num)
-		}
-	}
-
-	const handleUpdate = (index, num) => {
-		const newBoard = [...board]
-		newBoard[index] = num
-		setBoard(newBoard)
-	}
-
 	const handleKeyDown = (e) => {
 		switch (e.key) {
 			case "ArrowDown":
 				setDirection("down")
-				nextRound()
 				break;
 			case "ArrowUp":
 				setDirection("up")
-				nextRound()
 				break;
 			case "ArrowLeft":
 				setDirection("left")
-				nextRound()
 				break;
 			case "ArrowRight":
 				setDirection("right")
-				nextRound()
 				break;
 			default:
 				break;
 		}
+		//nextRound()
 	}
 
 	return (
 		<div tabIndex="0" onKeyDown={handleKeyDown} className="flex flex-col justify-evenly items-center h-screen border-0">
 			<>
-			{!gameOver && <Board board={board} setBoard={setBoard}/>}
+			{!gameOver && <Board  wait={wait} setWait={setWait} direction={direction} setDirection={setDirection} board={board} setBoard={setBoard}/>}
 			<div>
 				<Button name="Left" />
 				<Button name="right" />
 				<Button name="up" />
 				<Button name="down" />
 				<Button name="reset" handleClick={resetGame} />
-				<Button name="next" handleClick={nextRound} />
+				<Button name="next" />
 			</div>
 			</>
 		</div>
 	);
 }
+
+const useTimeout = (
+	callback, // function to call. No args passed.
+	// if you create a new callback each render, then previous callback will be cancelled on render.
+	timeout= 0, // delay, ms (default: immediately put into JS Event Queue)
+  ) => {
+	  const timeoutIdRef = useRef();
+	  const cancel = useCallback(
+		  () => {
+			  const timeoutId = timeoutIdRef.current;
+			  if (timeoutId) {
+				  timeoutIdRef.current = undefined;
+				  clearTimeout(timeoutId);
+			  }
+		  },
+		  [timeoutIdRef],
+	  );
+  
+	  useEffect(
+		  () => {
+			timeoutIdRef.current = setTimeout(callback, timeout);
+			return cancel;
+		  },
+		  [callback, timeout, cancel],
+	  );
+  
+	  return cancel;
+  }
 
 const generateInitialBoard = () => {
 	let num = Math.floor(Math.random() * 16)
@@ -191,6 +194,46 @@ const generateInitialBoard = () => {
 const generateRandomNum = () => {
 	const numIsTwo = Math.random() >= 0.5
 	return numIsTwo ? 2 : 4
+}
+
+const addRow = (row) => {
+	let current = 0
+	let currentIdx = 0
+	row.forEach((element, idx) => {
+		if (current === element) {
+			row[idx] = null
+			row[currentIdx] = current * 2
+			current = 0
+			currentIdx = 0
+		}
+		if (element !== null) {
+			current = element
+			currentIdx = idx
+		}
+	});
+}
+
+const moveRow = (row, move) => {
+	if (move === "left" || move === "up") {
+		row.sort((a,b) => {
+			if (b>a) 
+			  return 0
+			if (b==null)
+			  return -1
+			else
+				return 0
+		});		
+	}
+	if (move === "right" || move === "down") {
+		row.sort((a,b)=>{
+			if (a>b)
+			  return 0
+			if (a==null)
+			  return -1
+			else
+				return 0		
+		});
+	}
 }
 
 
